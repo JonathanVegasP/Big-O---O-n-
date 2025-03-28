0908cafc327111d1a980c5c540ed6a0a35ccb448
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2025-03-28 16:44:34 -0300

    added big o(1)

---
+++ o(1).js
@@ -0,0 +1,6 @@
+// Big O Complexity:
+// The function only performs a fixed number of operations regardless of the value of n.
+// Since the operations are constant (three additions), the time complexity is O(1).
+function sumNumber(n) {
+    return n + n + n
+}
